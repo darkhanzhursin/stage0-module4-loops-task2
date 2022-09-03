c9ba37ca4441037639b338eb2c66d957d434cd5d
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-09-03 21:06:09 +0600

    Task #2. While Loops

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+
+        int counter = 0;
+
+        byte minus = -1;
+
+        if (multiplyByAndToInclusive > 0) {
+            while (counter <= multiplyByAndToInclusive) {
+                System.out.println(counter * multiplyByAndToInclusive);
+                counter++;
+            }
+        } else if (multiplyByAndToInclusive < 0) {
+            while (counter >= multiplyByAndToInclusive) {
+                System.out.println(counter * (multiplyByAndToInclusive*minus));
+                counter--;
+            }
+        }
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+
+        int counter = 1;
+        int primeCheckNumber;
+        int divisibleCount;
+
+        while (counter <= printToInclusive) {
+            divisibleCount = 0;
+            primeCheckNumber = 2;
+            while (primeCheckNumber <= counter/2) {
+                if (counter % primeCheckNumber == 0) {
+                    divisibleCount++;
+                    break;
+                }
+                primeCheckNumber++;
+            }
+            if (divisibleCount == 0 && counter != 1) {
+                System.out.println(counter);
+            }
+            counter ++;
+        }
     }
 }
